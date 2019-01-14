e24bdcf435ba2b8686d234a69238ed48ff80f117
4G-IT <4G-IT@INFO1-PC12>
2019-01-14 13:18:17 +0100

    dsadsa

---
+++ Pizzeria/src/Menu.java
@@ -0,0 +1,47 @@
+
+public class Menu {
+	private int i =0;
+	Pizza [] arrayP=new Pizza[20];
+	Pizza pizz;
+	Pizza[] arrayNo= new Pizza[20];
+	private int k=0;
+	
+	
+	public void inserisciPizza(Pizza p) {
+		if (i>arrayP.length) {
+			System.out.println("hai superato il max");
+		} else {
+				arrayP[i]=p;
+			i++;
+			arrayP[1].ingredienti[1]="aa";
+
+		}
+		
+	}
+	public void trovaPizzeSenza(String s) {
+		for(int q=0;q<i;q++) {
+			System.out.println(arrayP[q].getNome());
+			for (int x=0;x<pizz.ingredienti.length;x++) {
+				System.out.println(arrayP[q].getIngredienti()[x]);
+			}
+		}
+		for (int y=0;y<i;y++) {	
+
+		
+		for (int x=0;x<pizz.ingredienti.length;x++) {
+			if (s==arrayP[y].ingredienti[x]) {
+				
+			} else {
+				arrayNo[k]=arrayP[y];
+				k++;
+			}
+			
+		}
+		}
+		for (int u=0;u<k;u++) {
+			System.out.println(arrayNo[k].toString());
+		}
+		
+	}
+	
+}
